0966ba93172377c8f7b2890c2a7750e1f7e7dd53
tayfun <tayfunemrekahraman@gmail.com>
2023-08-08 14:25:41 +0300

    added Services section

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,11 @@
 import { useEffect, useState } from 'react'
+import Image from 'next/image';
 import Layout from '../components/Layout'
-
+import { PiDesktopTowerBold } from 'react-icons/pi';
+import { BiServer, BiCctv } from 'react-icons/bi';
+import { GrHostMaintenance } from 'react-icons/gr';
+import { AiOutlineInfoCircle } from 'react-icons/ai';
+import { SlWrench } from 'react-icons/sl';
 
 export default function Home() {
   const [windowWidth, setWindowWidth] = useState(0);
@@ -72,9 +77,9 @@ export default function Home() {
   }
 
   const explanationSizeSetter = () => {
-    if(windowWidth >= 1000) return 'text-3xl'
-    else if(windowWidth >= 700) return 'text-2xl'
-    else if(windowWidth >= 500) return 'text-xl'
+    if (windowWidth >= 1000) return 'text-3xl'
+    else if (windowWidth >= 700) return 'text-2xl'
+    else if (windowWidth >= 500) return 'text-xl'
     else return 'text-lg'
   }
 
@@ -85,9 +90,38 @@ export default function Home() {
         <h3 id='title-explanation' className={`${explanationSizeSetter()} text-3xl text-gray-200 mt-5`}>-Kaliteli ve güvenilir hizmet-</h3>
       </div>
 
-      <main className="shadow absolute top-[60%] w-full h-full bg-gray-200 py-32 rounded-3xl overflow-hidden z-10">
-        <section>
-          kamera sistemleri
+      <main className="shadow absolute top-[60%] bg-gray-200 py-32 rounded-3xl z-10">
+        <section id='services'>
+          <h2 className='text-center text-4xl font-semibold text-red-600 underline underline-offset-4'>Hizmetlerimiz</h2>
+          <div className='flex justify-evenly items-center flex-wrap bg-red-200 w-11/12 mx-auto mt-12 py-10 rounded-3xl'>
+            <div className='service-card'>
+              <div className='service-card-text'><PiDesktopTowerBold /><span>Donanım Satışı</span></div>
+              <Image src="/images/img1.png" className='mx-auto' height="300" width="300" alt='server image' />
+            </div>
+            <div className='service-card'>
+              <div className='service-card-text'><BiServer /><span>Network Çözümleri</span></div>
+              <Image src="/images/img2.jpg" className='mx-auto' height="300" width="300" alt='switch image' />
+            </div>
+            <div className='service-card'>
+              <div className='service-card-text'>
+                <style>{`svg path {stroke: rgb(220 38 38)}`}</style>
+                <GrHostMaintenance /><span>Bakım Anlaşmaları</span>
+              </div>
+              <Image src="/images/img3.png" className='mx-auto' height={200} width={200} alt='shaking hands image' />
+            </div>
+            <div className='service-card'>
+              <div className='service-card-text'><AiOutlineInfoCircle /><span>Danışmanlık Hizmetleri</span></div>
+              <Image src="/images/img4.jpeg" className='mx-auto' height={300} width={300} alt='consultancy image' />
+            </div>
+            <div className='service-card'>
+              <div className='service-card-text'><SlWrench /><span>Teknik Servis</span></div>
+              <Image src="/images/img5.png" className='mx-auto' height={300} width={300} alt='wrench and screwdriver image' />
+            </div>
+            <div className='service-card'>
+              <div className='service-card-text'><BiCctv /><span>Güvenlik Sistemleri</span></div>
+              <Image src="/images/img6.png" className='mx-auto' height={300} width={300} alt='cctv image' />
+            </div>
+          </div>
         </section>
       </main>
     </Layout>
